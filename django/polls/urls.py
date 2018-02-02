10e3524ab3b442b95cd5aadfb30db4bb16fd7ba5
ShinJongMin <standbyme227@gmail.com>
2018-02-02 15:28:22 +0900

    detail.view choice 목록 출력

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    
+
     path(r'^$',views.index, name='index'),
     path(r'^(?P<question_id>\d+)/$',views.detail, name='detail'),
     path(r'^(?P<question_id>\d+)/results/$',views.results, name='results'),
--- django/polls/views.py
+++ django/polls/views.py
@@ -1,17 +1,42 @@
 from django.shortcuts import render
-# Create your views here.
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
+# from django.template import loader
+
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # 가장 최근에 발행된 최대 5개의 Question목록
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # 쉼표단위로 구분된 Question목록의 각 항목의 question_text로 만들어진 문자
+    # output = ', '.join([q.question_text for q in latest_question_list])
+
+    context = {
+        'latest_question_list': latest_question_list
+    }
+
+    return render(request=request, template_name='polls/index.html', context=context, )
+
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+
+    question = Question.objects.get(pk=question_id)
+    context = {
+        'question' : question
+    }
+
+    return render(request=request, template_name='polls/detail.html', context=context, )
+
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're coting on quesion %s" % question_id)
